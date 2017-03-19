3839d0b23a1998568a53c410c540f3f599af6670
Luis Martins <lg.martins@campus.fct.unl.pt>
2017-03-19 20:08:30 +0000

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = () => {
+  return(
+    <div>
+      <IndexLink to="/" activeClassName="active-link">React Timer App</IndexLink> |
+      <Link to="/timer" activeClassName="active-link">Timer</Link> |
+      <Link to="/countdown" activeClassName="active-link">Countdown</Link>
+    </div>
+  );
+};
+
+module.exports = Nav;
